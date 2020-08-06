eab3a073143534360a1bb11215ae58bc065ce78b
SajinKowserSK <sajinkowser@gmail.com>
2020-08-06 15:57:21 -0400

    weighted values matrix calculation

--- flask-backend/brute_force_script.py
+++ flask-backend/brute_force_script.py
@@ -1,9 +1,6 @@
 from auth_class import *
 import requests
 import pandas as pd
-
-
-
 from urllib.parse import urlencode
 
 ## Stuff to put in git ignore file when making repo public
@@ -33,17 +30,24 @@ track_id = [
 
 i = 0
 
+# just using these as dummy values rn so I don't have to add in user input everytime (commented out user rate)
+stars = [4,5]
+songNum = 0
 for song in track_id:
     r = requests.get(endpoint + song, headers=auth_header)
+
     names =  []
 
     #we would get this answer from the front end.
-    rate = int(input("What do you rate this song, out of 5: "))
+    # rate = int(input("What do you rate this song, out of 5: "))
 
+    rate = stars[songNum]
     data = []
+
     for keys in r.json():
         if(keys=="type"):
             break
+
         names.append(keys)
         data.append(float(r.json()[keys]))
 
@@ -56,8 +60,28 @@ for song in track_id:
         df2 = pd.DataFrame([[data[0]*rate, data[1]*rate, data[2]*rate, data[3]*rate, data[4]*rate, data[5]*rate, data[6]*rate, data[7]*rate, data[8]*rate, data[9]*rate, data[10]*rate]],columns=names)
         df = df.append(df2, ignore_index=True)
 
-print(df)
-print(df.sum(axis=0))
+    songNum += 1
 
+## now we have a user profile (unweighted)
+print(df)
+
+user_profile = df.sum(axis=0)
+user_profile = pd.DataFrame(user_profile, columns=['Values'])
+user_profile['Weighted_Values'] = 0
+
+# gets the total of values, we must now divide each feature val by this total
+user_profile_totals = user_profile['Values'].sum()
+
+rowNum = 0
+NEW_COL = 1
+OLD_COL = 0
+
+# creating a new column with the weighted values for each feature based on user profile
+for index, row in user_profile.iterrows():
+    user_profile.iloc[rowNum, NEW_COL] = user_profile.iloc[rowNum, OLD_COL] / user_profile_totals
+    rowNum += 1
+
+print(user_profile)
+print('\n', user_profile['Weighted_Values'].sum())
 
 

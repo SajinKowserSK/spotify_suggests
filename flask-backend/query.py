e9bc814ce527db877d8b377c1188ef7282e8ea12
SajinKowserSK <sajinkowser@gmail.com>
2020-08-15 17:53:56 -0400

    finished dict with track name and id

--- flask-backend/query.py
+++ flask-backend/query.py
@@ -2,21 +2,35 @@ from auth_class import *
 import sys
 import requests
 
-response = spotify.search("Rap Caviar", search_type="playlist")['playlists']['items']
+tracksList = []
+test = [("RapCaviar", "37i9dQZF1DX0XUsuxWHRQd"),
+        ("Most Necessary", "37i9dQZF1DX2RxBh64BHjQ"),
+        ("Gold School", "37i9dQZF1DWVA1Gq4XHa6U"),
+        ("Signed XOXO", "37i9dQZF1DX2A29LI7xHn1"),
+        ("Feelin' Myself", "37i9dQZF1DX6GwdWRQMQpq"),
+        ("I Love My '80s Hip-Hop", "37i9dQZF1DX2XmsXL2WBQd"),
+        ("I Love My Down South Classics", "37i9dQZF1DWYok9l1JL7GM")
+        ]
 
-## UNCOMENT TO GET THE ALBUM NAME, WITH ID AND TRACKS
+for elem in test:
+    track = elem[0]
+    track_id = elem[1]
+    response = spotify.search(track, search_type="playlist")['playlists']['items']
 
-# print(response)
-# print(len(response))
+    for key in response:
+        if key['name'] == track and key['id'] == track_id:
+            tracksList.append(key['tracks']['href'])
 
-# for key in response:
-#     print(key['name'])
-#     print(key)
+print(tracksList)
+trackDict = {}
+for tracks in tracksList:
+    tracks = "https://api.spotify.com/v1/playlists/37i9dQZF1DX0XUsuxWHRQd/tracks"
+    r = requests.get(tracks, headers=spotify.get_resource_header())
+    r = r.json()['items']
+    for key in r:
+        track = key['track']
+        if track is not None:
+            print(track['name'])
+            trackDict[track['name']] = track['id']
 
-tracks = "https://api.spotify.com/v1/playlists/37i9dQZF1DX0XUsuxWHRQd/tracks"
-r = requests.get(tracks, headers=spotify.get_resource_header())
-r = r.json()['items']
-for key in r:
-    track = key['track']
-    if track is not None:
-        print(track['name'])
+print(trackDict)

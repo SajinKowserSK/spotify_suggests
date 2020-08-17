3a6ee6766c1f025351fc82c3357f35395b6be1e8
dg022 <dgeorg6@uwo.ca>
2020-08-16 19:09:33 -0600

    adding the fire animations

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Player from './Player';
 import Stars from './Stars';
 import './App.css';
+import ParticlesBg from "particles-bg";
 
 class App extends React.Component {
 
@@ -13,21 +14,24 @@ class App extends React.Component {
   render()
   {
   return (
+<div>
+<ParticlesBg color="#1DB954" type="circle" bg={true}/>
 
 <div id="c"class="ui card">
   <div class="content">
-  
+
     <div class="header">Toosie Slide - Drake</div>
     <div class="description">
       <Player song={this.state.song} />
     </div>
   </div>
    <div class="extra content">
-    <center> Rate the song out of 5!</center> 
+    <center> Rate the song out of 5!</center>
       <Stars/>
 
   </div>
- 
+
+</div>
 </div>
   );
 
